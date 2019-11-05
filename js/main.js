f030736e60ee738606ef38d4349c8273885f7f17
Jurgitamik <56818575+Jurgitamik@users.noreply.github.com>
2019-11-05 12:54:42 +0200

    6 intro

---
+++ js/main.js
@@ -0,0 +1,86 @@
+console.log('labas')
+
+// skaiciai - number (isvedem i konsole - rasom log ir t.t.)
+const PI = 3.141592654;
+console.log( PI );
+
+const nulis = 0;
+
+// tekstai - string (let - vieta, suo - gyventojas, butinai rasyti kabutese)
+const vardas = 'Jurgita';
+let suo = "Aviganis";
+console.log( suo );
+suo = 'Coli';
+console.log( suo );
+
+// logines reiksmes - boolean
+const tiesa = true;
+console.log( tiesa );
+
+const melas = false;
+console.log( melas );
+
+
+// sarasai - array (kablelis ir tarpas tarp elementu, gali buti ir tuscias, tekstai rasomi kabutese)
+const skaiciai = [1, 2, 3, 4];
+const abc = ['a', 'b', 'c']
+console.log( abc );
+
+
+// kintamasis - variable (kai kur gali buti naudojama 'let')
+var kintamasis = 'as kintu :)';
+
+// misrus sarasai
+const mixList = [1, 'antras', 3, 'forth', [6, 'seven']];
+console.log( mixList );
+
+//objektai
+const zmogus = {
+    name: 'Vardenis',
+    surname: 'Pavardenis',
+    age: 99
+}
+console.log( zmogus );
+
+console.log( '.....................' );
+console.log( '.....................' );
+console.log( '.....................' );
+console.log( 'veiksmai su kintamaisiais' );
+
+const s1 = 5;
+const s2 = 10;
+
+const suma = s1 + s2;
+console.log( suma );
+
+const vidurkis = (s1 + s2) / 2;( vidurkis );
+
+
+
+
+const vidurkiosaknis = Math.sqrt (vidurkis);
+console.log(vidurkiosaknis);
+
+const t1 = 'Labas';
+const t2 = 'rytas';
+const pasisveikinimas = t1 + t2;
+console.log(pasisveikinimas);
+
+const gerasPasisveikinimas = t1 + ' ' + t2;
+console.log(gerasPasisveikinimas);
+
+
+const numbers = [10, 7, 3, -4, 6, 24];
+console.log(numbers);
+console.log( numbers[2]);
+
+const alter = numbers[0] - numbers[1] + numbers[2] - numbers[3] + numbers[4];
+console.log(alter);
+
+
+
+
+
+ 
+
+
